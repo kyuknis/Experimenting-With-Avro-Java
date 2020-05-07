6ae2f931620e910fc7e60fa80ced317e0161243f
Kirkland Yuknis <kirklandyuknis@Kirklands-MBP.localdomain>
2020-05-07 09:43:46 -0400

    Added Java files

---
+++ src/main/java/com/yuknis/experimentingwithavro/ExperimentingWithAvro.java
@@ -0,0 +1,15 @@
+package com.yuknis.experimentingwithavro;
+
+import org.apache.log4j.Logger;
+
+public class ExperimentingWithAvro {
+
+    private static final Logger logger = Logger.getLogger(ExperimentingWithAvro.class);
+
+    public static void main(String[] args) {
+
+        logger.info("In main()");
+
+    }
+
+}
